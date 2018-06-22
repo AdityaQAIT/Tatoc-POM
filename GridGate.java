f394e3d75fe84952308c5e68fafd2a8155781557
AdityaQAIT <40289585+AdityaQAIT@users.noreply.github.com>
2018-06-22 09:19:49 +0530

    Add files via upload

---
+++ GridGate.java
@@ -0,0 +1,34 @@
+package com.qainfotech.automation.tatoc;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class GridGate {
+	
+	WebDriver driver;
+	
+	public GridGate(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+	public FrameDungeon ClickOnGreenBox() {
+		driver.findElement(By.className("greenbox")).click();
+		return new FrameDungeon(driver);
+
+	}
+	
+	public boolean isGridGatePage() {
+		return driver.getCurrentUrl().equals("http://10.0.1.86/tatoc/basic/grid/gate");
+	}
+	
+	public boolean isErrorByClickOnRedbox() {
+		driver.findElement(By.className("redbox")).click();
+		String url = driver.getCurrentUrl();
+		driver.navigate().back();
+		return url.equals("http://10.0.1.86/tatoc/error");
+		
+	}
+	
+
+}
